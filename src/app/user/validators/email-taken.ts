2e9785a2bce78070cb136aaf1b74e85d31a3743d
IzzeldeenAsh <ezraashour00@gmail.com>
2023-02-10 09:57:10 +0300

    nameUpdate

--- src/app/user/validators/email-taken.ts
+++ src/app/user/validators/email-taken.ts
@@ -1,18 +1,22 @@
-import { AngularFireAuth } from "@angular/fire/compat/auth";
-import { Injectable } from "@angular/core";
-import { AbstractControl, AsyncValidator, ValidationErrors } from "@angular/forms";
-import { Observable } from "rxjs";
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Injectable } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidator,
+  ValidationErrors,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class EmailTaken implements AsyncValidator{
-  constructor(private auth:AngularFireAuth){
+export class EmailTaken implements AsyncValidator {
+  constructor(private auth: AngularFireAuth) {}
 
-  }
-
-  validate = (control : AbstractControl):Promise<ValidationErrors|null> => {
-     return this.auth.fetchSignInMethodsForEmail(control.value).then((res)=> res.length ?  {emailTaken:true} : null)
-  }
+  validate = (control: AbstractControl): Promise<ValidationErrors | null> => {
+    emailTaken: false;
+    return this.auth.fetchSignInMethodsForEmail(control.value).then((res) => {
+      return res.length ? { emailTaken: true } : null;
+    });
+  };
 }
